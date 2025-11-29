a53db7456e5250b2a65ff3f94474b83f01da721e
Abdulbasit908 <btvice12@gmail.com>
2025-11-29 14:17:40 +0500

    Your commit message describing changes

--- src/components/Dashboard/EmployeeDashboard.jsx
+++ src/components/Dashboard/EmployeeDashboard.jsx
@@ -3,7 +3,9 @@ import Header from '../employeeSmallComp/Header'
 import TaskRender from '../employeeSmallComp/TaskRender'
 import TaskList from '../Tasklist/TaskList'
 
-const EmployeeDashboard = ({handleLogout, userData}) => {
+const EmployeeDashboard = ({handleLogout, userData, userName}) => {
+  
+  
   return (
     <div className=' bg-[#1C1C1C] p-8 min-h-screen'>
       <Header handleLogout={handleLogout} userName={userName} userData={userData}/>
